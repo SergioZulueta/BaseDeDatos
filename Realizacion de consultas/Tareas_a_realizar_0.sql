3514015068b510db510d6819b25cb9d965763be6
Sergio Zulueta <sergio.zulueta@ikasle.egibide.org>
2018-12-05 13:47:08 +0100

    UPDATES

---
+++ Realizacion de consultas/Tareas_a_realizar_0.sql
@@ -0,0 +1,32 @@
+--1 
+/*Seleccionar de la tabla EMPLE aquellos empleados del departamento 20 
+y cuyo oficio sea ‘ANALISTA’. La consulta se ha de ordenar de modo descendente por
+APELLIDO y también de manera descendente por número de empleado.
+*/
+SELECT APELLIDO 
+FROM EMPLE
+WHERE DEPT_NO = 20
+AND UPPER(OFICIO) = 'ANALISTA'
+ORDER BY APELLIDO, EMP_NO DESC;
+
+
+--2
+/*Mostrar el apellido, oficio y fecha de alta de los empleados contratados entre los
+meses de mayo y setiembre del año 1981. Obtener la salida ordenada por fecha de alta.
+*/
+SELECT APELLIDO, OFICIO, FECHA_ALTA
+FROM EMPLE
+WHERE FECHA_ALTA BETWEEN TO_DATE('01/05/1981', 'DD/MM/YYYY') AND TO_DATE('30/09/1981', 'DD/MM/YYYY');
+
+
+--3
+/* Mostrar los apellidos de los empleados concatenados con el oficio, 
+separados por una coma y un espacio en blanco y ordenados por el apellido. 
+Llamar a la columna 'EMPLE y OFI'.
+*/
+SELECT APELLIDO || ', '|| OFICIO AS "EMPLE Y OFI"
+FROM EMPLE
+ORDER BY APELLIDO;
+
+
+
