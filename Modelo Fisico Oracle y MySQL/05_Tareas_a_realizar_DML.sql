6cff49e53da45eb1f9ef6aa083ffc19148943354
Sergio Zulueta <sergio.zulueta@ikasle.egibide.org>
2018-12-13 10:53:55 +0100

    subir updates

---
+++ Modelo Fisico Oracle y MySQL/05_Tareas_a_realizar_DML.sql
@@ -0,0 +1,34 @@
+--1
+/*Añadir un nuevo departamento: 'Calidad', con localización Santander y código 11.
+Añadir un empleado vinculado al departamento recién creado sus datos son:  7230, Aguirre, empleado.*/
+
+INSERT INTO DEPART (DNOMBRE, LOC, DEPT_NO) VALUES ('CALIDAD', 'SANTANDER',11);
+
+
+SELECT * FROM DEPART;
+
+--2
+/*Aplicar un recorte presupuestario del 10% a todos los sueldos.*/
+
+
+--3
+/*?	Reasignar a los empleados del departamento de contabilidad (código 10) al departamento de producción  (código 40).
+*/
+
+--4
+/*?	Modificar a todos los empleados cuyo director es el  7566, pasan a ser subordinados del jefe de tovar.
+*/
+
+--5
+/*?	A los trabajadores cuyo oficio sea empleado se les adjudicara el mismo sueldo que tiene      MUÑOZ.
+*/
+
+--6
+/*?	Todos los trabajadores cuyo departamento este localizado en Santander tendrán una comisión de 300€.
+*/
+
+--7
+/*?	Despedir a todos los empleados que trabajan para el departamento de ventas (código 30).
+*/
+
+--7
