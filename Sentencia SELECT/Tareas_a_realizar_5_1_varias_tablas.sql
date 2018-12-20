15d531e20220d046edddebc97aa579b6f2576c75
Sergio Zulueta <sergio.zulueta@ikasle.egibide.org>
2018-12-20 10:58:19 +0100

    Ejercicio finalizado

--- Sentencia SELECT/Tareas_a_realizar_5_1_varias_tablas.sql
+++ Sentencia SELECT/Tareas_a_realizar_5_1_varias_tablas.sql
@@ -18,23 +18,40 @@ AND UPPER(LOC) IN ('MADRID', 'BARCELONA');
 
 --3
 /*Obtener para cada empleado (código y apellidos), así como los apellidos de su jefe. 
-La columna JEFE de un empleado indica el numero de empleado de su jefe.
-*/
+La columna JEFE de un empleado indica el numero de empleado de su jefe.*/
 
-SELECT E.EMP_NO, E.APELLIDO
-FROM EMPLE E, DEPART D
-WHERE E.DEPT_NO = D.DEPT_NO
-AND ;
+SELECT E.EMP_NO, E.APELLIDO, J.APELLIDO AS JEFE
+FROM EMPLE E, EMPLE J
+WHERE E.ID_JEFE = J.EMP_NO
+ORDER BY J.APELLIDO;
 
 --4
-/*Visualizar el apellido, el oficio y la localidad de los departamentos donde trabajan los ANALISTAS.
-*/
+/*Visualizar el apellido, el oficio y la localidad de los departamentos donde trabajan los ANALISTAS.*/
+
+SELECT APELLIDO, OFICIO, LOC
+FROM EMPLE E, DEPART D
+WHERE E.DEPT_NO = D.DEPT_NO
+AND UPPER(OFICIO) = 'ANALISTA';
+
 
 --5
-/*Mostrar para cada empleado quién es su jefe (apellido ). Si no tiene jefe, mostrar ausencia de valor(NULL).
-*/
+/*Mostrar para cada empleado quién es su jefe (apellido ). Si no tiene jefe, mostrar ausencia de valor(NULL).*/
+
+SELECT E.EMP_NO, E.APELLIDO, J.APELLIDO AS JEFE
+FROM EMPLE E, EMPLE J
+WHERE E.ID_JEFE = J.EMP_NO(+)
+ORDER BY J.APELLIDO;
+
+SELECT E.EMP_NO, E.APELLIDO, NVL(J.APELLIDO,'NO TIENE JEFE') AS JEFE
+FROM EMPLE E, EMPLE J
+WHERE E.ID_JEFE = J.EMP_NO(+)
+ORDER BY J.APELLIDO;
 
 --6
 /*Mostrar para todos los departamentos que tenemos en la empresa los apellidos de los empleados 
-que pertenecen a cada uno de ellos. Si algún departamento está vacío, mostrar ausencia de valor NULL.
-*/
+que pertenecen a cada uno de ellos. Si algún departamento está vacío, mostrar ausencia de valor NULL.*/
+
+SELECT APELLIDO, D.DEPT_NO
+FROM EMPLE E, DEPART D
+WHERE E.DEPT_NO(+) = D.DEPT_NO
+ORDER BY DEPT_NO;
